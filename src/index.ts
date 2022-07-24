5f70b5ade24e00fe3833fbaa1bb93e4780da12bc
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2022-07-24 01:06:03 -0300

    Bugs corrigidos e demais endpoints criados

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from "./app"
 import {createStudent} from "./endpoints/estudante/createStudent"
+import { changeStudentClass } from "./endpoints/estudante/changeStudentClass"
+import { getStudent } from "./endpoints/estudante/getStudent"
 import { buscarTurmaAtiva } from "./endpoints/turma/buscarTurmaAtiva"
 import { criarTurma } from "./endpoints/turma/criarTurma"
 import { mudarTurmaDeModulo } from "./endpoints/turma/mudarTurmaDeModulo"
@@ -18,8 +20,11 @@ app.put("/turma/editar", mudarTurmaDeModulo)
 
 
 //Pegar estudante pelo nome
-// app.get("/estudante/:nome", getStudent)
+app.get("/estudante", getStudent)
 
 
 //Criar estudante
 app.post("/estudante/criar", createStudent)
+
+//Mudar estudante de turma
+app.patch("/estudante", changeStudentClass)
